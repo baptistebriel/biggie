ab52cab653a9a369b776b9482542aa8105e4052e
Baptiste Briel <baptiste@sa-studio.fr>
2015-09-01 10:46:10 +0200

    Update watch.js
    watch for all less files changes

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 
 gulp.task('watch', function() {
-    gulp.watch('assets/less/layout.less', ['less'])
+    gulp.watch('assets/less/**/*.less', ['less'])
     gulp.watch('assets/js/**/*.js', ['js']);
 });
