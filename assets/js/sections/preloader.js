a6a6d2d4b950c02cf20b365428855bcf7af4b4cd
Baptiste Briel <baptiste@sa-studio.fr>
2015-09-07 11:03:35 +0200

    Added Preloader

---
+++ assets/js/sections/preloader.js
@@ -0,0 +1,72 @@
+var config = require('../config');
+var utils = require('../utils');
+var Tween = require('gsap');
+
+function Preloader(onComplete) {
+    
+    this.preloaded = onComplete;
+    this.view = config.$view;
+    this.el = null;
+
+};
+
+Preloader.prototype = {
+    
+    init: function(req, done) {
+        
+        var self = this;
+        
+        this.createDOM();
+        
+        done();
+        
+    },
+
+    createDOM: function() {
+        
+        var page = this.view.firstChild;
+
+        this.el = utils.createEl({
+            selector: 'div',
+            styles: 'preloader'
+        });
+        
+        this.view.insertBefore(this.el, page);
+        
+    },
+
+    resize: function(width, height) {},
+    
+    animateIn: function(req, done) {
+
+        var self = this;
+        
+        var tl = new TimelineMax({ paused: true, onComplete: function(){
+            done();
+            // call this.preloaded to bring the first route
+            self.preloaded();
+        }});
+        tl.to(this.el, 1, {autoAlpha: 1});
+        tl.restart();
+        
+    },
+    
+    animateOut: function(req, done) {
+
+        var tl = new TimelineMax({ paused: true, onComplete: done });
+        tl.to(this.el, 1, {autoAlpha: 0});
+        tl.restart();
+        
+    },
+    
+    destroy: function(req, done) {
+
+        this.view.removeChild(this.el);
+        
+        done();
+    
+    }
+
+};
+
+module.exports = Preloader;
