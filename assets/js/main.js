516dbfa783429dcff9d3595a269b3a8b49d48084
BaptisteBriel <baptiste@buildinamsterdam.com>
2015-09-11 16:00:28 +0200

    refactor utils, added defaultEase for GSAP

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,14 @@
 biggie
 main entry point
 ---------- */
-/* ----------
-require
----------- */
 var config = require('./config');
 var framework = require('./framework');
+
+/* ----------
+gsap
+---------- */
+var Tween = require('gsap');
+TweenLite.defaultEase = Expo.easeOut;
 
 /* ----------
 init bigwheel framework
