e31078772de2c401df62fe390ab14f6ba67fcf43
Baptiste Briel <baptiste@sa-studio.fr>
2015-09-14 10:32:31 +0200

    Update javascript.js

--- gulp/tasks/javascript.js
+++ gulp/tasks/javascript.js
@@ -1,15 +1,38 @@
-var gulp = require('gulp');
-var browserify = require('gulp-browserify');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
+'use strict';
 
-gulp.task('js', function() {
-    gulp.src('assets/js/*.js')
-        .pipe(browserify({
-          insertGlobals : true,
-          debug : !gulp.env.production
-        }))
-	   .pipe(uglify())
-	   .pipe(concat('app.min.js'))
-	   .pipe(gulp.dest('build/'))
-});
+var watchify = require('watchify');
+var browserify = require('browserify');
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var gutil = require('gulp-util');
+var maps = require('gulp-sourcemaps');
+var assign = require('lodash.assign');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+var customOpts = {
+    entries: ['assets/js/main.js'],
+    debug: true
+};
+
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts));
+
+gulp.task('js', bundle);
+b.on('update', bundle);
+b.on('log', gutil.log);
+
+function bundle() {
+  return b.bundle()
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('app'))
+    .pipe(rename({ extname: '.js' }))
+    .pipe(gulp.dest('./build/'))
+    .pipe(buffer())
+    .pipe(uglify())
+    // .pipe(maps.init({loadMaps: true}))
+    // .pipe(maps.write('./'))
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest('./build'))
+}
