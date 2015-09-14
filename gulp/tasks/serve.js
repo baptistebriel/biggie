590498b7ab3e02f6aaee7cd417c68333372bc390
Baptiste Briel <baptiste@sa-studio.fr>
2015-09-14 11:04:01 +0200

    Update and rename watch.js to serve.js
    Added browser-sync for live-reloading and CSS injection

---
+++ gulp/tasks/serve.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('serve', ['less', 'js'], function() {
+	
+	browserSync({
+		notify: false,
+		server: {baseDir: './'}
+	});
+
+	gulp.watch('assets/less/**/*.less', ['less']);
+	gulp.watch('assets/js/**/*.js', ['js']);
+
+	gulp.watch(['*.html', 'build/*.css', 'build/*.js'], {cwd: ''}, reload);
+	
+});
--- gulp/tasks/watch.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-
-gulp.task('watch', function() {
-    gulp.watch('assets/less/**/*.less', ['less'])
-    gulp.watch('assets/js/**/*.js', ['js']);
-});
