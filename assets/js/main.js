9ba7873af2e43a066e5bdc9abb9328aeb7e3a781
Baptiste Briel <baptiste@sa-studio.fr>
2015-09-28 13:24:59 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,7 @@
 biggie
 main entry point
 ---------- */
-var config = require('./config');
 var framework = require('./framework');
-
-/* ----------
-gsap
----------- */
-var Tween = require('gsap');
-TweenLite.defaultEase = Expo.easeOut;
 
 /* ----------
 init bigwheel framework
