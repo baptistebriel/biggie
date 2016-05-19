01836d27ce4993b64a0168a759a46e463306a653
mbusson <mbusson@users.noreply.github.com>
2016-05-20 00:44:49 +0200

    Update styles.js

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -2,15 +2,15 @@
 
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var less = require('gulp-less');
+var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var cleancss = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var browserSync = require('browser-sync');
 
-gulp.task('less', function() {
-    gulp.src('assets/less/layout.less')
-        .pipe(less().on('error', gutil.log))    
+gulp.task('sass', function() {
+    gulp.src('assets/scss/layout.scss')
+        .pipe(sass().on('error', gutil.log))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
@@ -19,4 +19,15 @@ gulp.task('less', function() {
         .pipe(rename("app.min.css"))
         .pipe(gulp.dest('build/'))
         .pipe(browserSync.stream());
+        
+        /* add new scss pages manually in a new task such as:
+        gulp.src(['assets/scss/anoterSassPage.scss', 'assets/scss/andAnotherOne.scss'])
+        .pipe(sass().on('error', handleErrors))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cleancss())
+        .pipe(gulp.dest('build/')) */
+
 });
