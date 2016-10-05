ffb74459ca4b0e3004c97f44d7cae7056e449c0b
Baptiste Briel <b.briel@suspended-animations.com>
2016-10-05 17:01:09 +0200

    fixed export of routes.js

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -4,7 +4,7 @@ import config from 'config'
 all routes needs to be defined inline
 see https://github.com/bigwheel-framework/documentation/blob/master/routes-defining.md#as-section-standard-form
 ---------- */
-export default {
+module.exports = {
 	[`${config.BASE}`]: require('./sections/home'),
 	[`${config.BASE}home`]: { section: require('./sections/home') },
 	[`${config.BASE}about`]: { section: require('./sections/about') },
